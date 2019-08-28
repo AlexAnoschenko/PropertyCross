42f21e538cd4a76c950f2a15deea0990f9213007
Alexey Anoschenko <elegalex@ya.ru>
2019-08-28 09:48:55 +0300

    tasks/task-2: Added ESlint

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
-
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello World!</h1>
+                <h1>Hello World!!!</h1>
             </div>
         );
     }
