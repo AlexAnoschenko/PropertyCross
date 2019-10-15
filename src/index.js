eacfa9861cb1cbfc2970eb44e300b2d4efcada84
Alexey Anoschenko <elegalex@ya.ru>
2019-10-15 14:58:47 +0300

    tasks/task-20: test IE11

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import 'react-app-polyfill/ie11';
 import store from './store/configureStore';
 import App from './components/App/App';
 
